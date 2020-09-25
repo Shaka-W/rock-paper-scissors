f9741bd28f20011b29e3c5fc32d9daaca5500031
Shaka-W <shakawillock@gmail.com>
2020-09-25 10:27:39 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-let computerChoice;
+let playerScore = document.querySelector('.player-score');
+let computerScore = document.querySelector('.computer-score');
+let scoreInfo = document.querySelector('.score-title');
+let scoreMsg = document.querySelector('.score-msg');
+
+let playerPoints = 0;
+let computerPoints = 0;
+
+let rockSelection = document.querySelector('.rock');
+let paperSelection = document.querySelector('.paper');
+let scissorsSelection = document.querySelector('.scissors');
+
+rockSelection.addEventListener('click', () => {
+   let playerSelection = 'rock';
+    playRound(playerSelection, computerPlay());
+});
+
+paperSelection.addEventListener('click', () => {
+    let playerSelection = 'paper';
+    playRound(playerSelection, computerPlay());
+});
+
+scissorsSelection.addEventListener('click', () => {
+   let playerSelection = 'scissors';
+    playRound(playerSelection, computerPlay());
+});
+
 function computerPlay() {
+    let computerChoice;
     let randomNumber = Math.floor(Math.random() * 3 + 1);
    
     if (randomNumber === 1) {
@@ -13,57 +40,52 @@ function computerPlay() {
     return computerChoice.toLowerCase();
 }
 
+function playRound(playerSelection, computerChoice) { 
+    if (playerSelection === 'rock' && computerChoice === 'scissors') {
+        scoreInfo.textContent = 'Computer throws scissors';
+        scoreMsg.textContent = 'Your point';
+        playerPoints = playerScore.innerHTML;
+        playerPoints++;
+        playerScore.innerHTML = playerPoints;
+    } else if (playerSelection === 'paper' && computerChoice === 'rock') {
+        scoreInfo.textContent = 'Computer throws rock';
+        scoreMsg.textContent = 'Your point';
+        playerPoints = playerScore.innerHTML;
+        playerPoints++;
+        playerScore.innerHTML = playerPoints;
+    } else if (playerSelection === 'scissors' && computerChoice === 'paper') {
+        scoreInfo.textContent = 'Computer throws paper';
+        scoreMsg.textContent = 'Your point';
+        playerPoints = playerScore.innerHTML;
+        playerPoints++;
+        playerScore.innerHTML = playerPoints;
+    } else if (playerSelection === computerChoice) {
+        scoreInfo.textContent = `Computer throws ${computerChoice}`;
+        scoreMsg.textContent = 'Draw';
+    } else {
+        scoreInfo.textContent = `Computer throws ${computerChoice}`;
+        scoreMsg.textContent = 'Computer\'s point';
+        computerPoints = computerScore.innerHTML;
+        computerPoints++;
+        computerScore.innerHTML = computerPoints;
+    }  
 
-
-let playerSelection;
-function userPlay() {
-    playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
-    
-    while(true) {
-        if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
-            return playerSelection;
-        } else {
-            playerSelection = prompt('Rock, Paper or Scissors?').toLowerCase();
-        }
+    while (playerPoints == 5 || computerPoints == 5) {
+        announceWinner(computerChoice);
     }
-}
+} 
 
-function playRound(playerSelection, computerPlay) { 
-        let msg;
-     
-        if (playerSelection === 'rock' && computerPlay === 'scissors') {
-            msg = 'You Win! Rock beats Scissors';
-        } else if (playerSelection === 'paper' && computerPlay === 'rock') {
-            msg = 'You Win! Paper beats Rock';
-        } else if (playerSelection === 'scissors' && computerPlay === 'paper') {
-            msg = 'You Win! Scissors beats Paper'
-        } else if (playerSelection === computerPlay) {
-            msg = "It's a Tie!"
-        } else {
-            msg = 'You Lose!';
-        }
-    
-        return msg;
-}
-
-function game(playRound) {
-    let computerScore = 0;
-    let userScore = 0;
-
-    for (let i = 1; i < 6; i++) {
-        let message = playRound(userPlay(), computerPlay());
-        
-        if(message.includes('Win')) {
-            ++userScore;
-            console.log(`You won this round. ${playerSelection} beats ${computerChoice} Current score is ` + userScore + ': ' + computerScore);
-        } else if (message.includes('You Lose!')) {
-            ++computerScore;
-            console.log(`You lost this round. ${computerChoice} beats ${playerSelection} Current score is ` + userScore + ': '+ computerScore);
-        } else if (message.includes('Tie')) {
-            console.log(`It's a tie. Current score is ` + userScore + ': ' + computerScore);
-        } 
+function announceWinner(computerChoice) {
+    if (playerPoints == 5) {
+          scoreInfo.textContent = `Computer throws ${computerChoice}`;
+          scoreMsg.textContent = `You win, ${playerPoints} to ${computerPoints}`;   
+    } else if (computerPoints == 5) {
+         scoreMsg.textContent = `You lose, ${playerPoints} to ${computerPoints}`;
     }
+
+    playerPoints = 0;
+    playerScore.innerHTML = playerPoints;
+
+    computerPoints = 0;
+    computerScore.innerHTML = computerPoints;
 }
-
-
-console.log(game(playRound));
